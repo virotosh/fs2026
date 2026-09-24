3c1cd035685dbc141bf8c53fe9cdf060b72d5a11
viro <vuonghiit@gmail.com>
2026-09-24 12:54:22 +0300

    mongo database created

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,24 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const router = express.Router();
+const mongoose = require("mongoose");
 
 dotenv.config();
+
+
+// connect mongodb atlas
+const connectDB = async () => {
+    try {
+       mongoose.set('strictQuery', false);
+       await mongoose.connect(process.env.MONGODB_URI);
+       console.log("MongoDB Connected");
+    } catch (error) {
+       console.error("Error connecting to MongoDB:", error);
+       process.exit(1);
+    }
+  };
+  connectDB();
+  
 
 const app = express();
 app.use(express.json());
